a3b006d0fc5249557351b8dc35dd944cc217b295
wosohd <wosohd@gmail.com>
2024-02-01 03:20:27 -0800

    Make change 2.0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,20 +2,23 @@
 
 """ Contains makeChange function"""
 
+
 def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    # Initialize a list to store the minimum number of coins needed for each total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    # If the value at total is still infinite, it means the total cannot be met
-    if dp[total] == float('inf'):
-        return -1
-
-    return dp[total]
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
